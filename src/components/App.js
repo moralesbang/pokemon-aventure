2d540b91f13a12dc802e2b385a1227baf956c960
Juan Fernando <juanferrgiraldo@gmail.com>
2018-08-28 13:04:48 -0500

    List pokemon names ready

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css'
+
+import Pokemon from './Pokemon';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       myPokemons: [],
-      pokemons: []
+      pokemons: []      
     }
   }
 
@@ -14,18 +17,37 @@ class App extends Component {
     // Consume the pokemons
   }
 
+  componentDidMount() {
+    axios.get('https://pokeapi.co/api/v2/pokemon/')
+    .then(response => {
+        const pokemons = response.data.results.slice(0, 10);
+        const updatePokemons = pokemons.map(pokemon => {                   
+          return {
+            ...pokemon            
+          }
+        })
+        this.setState({pokemons: updatePokemons});        ;
+    });
+  }
+
   render() {
+    const pokemons = this.state.pokemons.map((pokemon, index) =>{
+      return <Pokemon
+        id={index + 1}
+        key={pokemon.url}
+        name={pokemon.name}/>        
+    })
     return (
       <div className="App">
+        <h4 style={{textAlign: 'center'}}>Selecciona el pokemon que quieres atrapar</h4>
         <section className="section">
           <div className="container">
             <div className="columns is-centered">
               <div className="column is-8">
-                <h5 class="title is-5">
+                <h5 className="title is-5">
                   Pokemons Captured: {this.state.myPokemons.length}
                 </h5>
-
-                {/* Put pokemons here */}
+                {pokemons}
               </div>
             </div>
           </div>
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,13 +1,22 @@
-<div className="card">
-  <div className="card-image">
-  </div>
-  
-  <div className="card-content">
-  </div>
+import React from 'react';
 
-  <footer className="card-footer">
-    <button className="button is-primary">
-      Capture
-    </button>
-  </footer>
-</div>
+const pokemon = (props) => (
+    <div className="card">
+        <div className="card-image">
+        </div>
+    
+        <div className="card-content">
+            <h4>Nombre: {props.name}</h4>
+            <p>{props.experience}</p>
+        </div>
+
+        <footer className="card-footer">
+            <button className="button is-primary">
+            Capture
+            </button>
+        </footer>
+    </div>
+
+)
+
+export default pokemon;

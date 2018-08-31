e470d1b2b395606b5b13115f7c1fd45db75e5255
Juan Camilo Morales <camov1209@gmail.com>
2018-08-30 22:34:21 -0500

    Adds styles to pokemon card

--- src/components/App.js
+++ src/components/App.js
@@ -23,14 +23,14 @@ class App extends Component {
         <section className="section">
           <div className="container">
             <div className="columns is-centered">
-              <div className="column is-8">
+              <div className="column is-10">
                 <h4 className="title is-4">
                   Pokemons Captured: {this.state.pokedex.length}
                 </h4>
                 <h5 className="subtitle is-5">Selecciona el pokemon que quieres atrapar</h5>
 
                 <div className="pokemon-list">
-                  {this.state.pokemons.map((pokemon, index) => <Pokemon key={index + 1} url={pokemon.url} />)}
+                  {this.state.pokemons.map((pokemon, index) => <Pokemon key={index} url={pokemon.url} />)}
                 </div>
               </div>
             </div>
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-
 class Pokemon extends Component {
   
   constructor(props) {
@@ -8,28 +7,45 @@ class Pokemon extends Component {
     this.state = { attributes: [] }
   }
 
-  componentWillMount() {
+  componentDidMount() {
     const requestPokemonAwait = async () => {
       const response = await fetch(this.props.url)
-      const json = await response.json();
-      this.setState({attributes: json})
+      const pokemon_attrs = await response.json()
+      this.setState({attributes: pokemon_attrs})
     }
     requestPokemonAwait()
-    console.log(this.setState)
   }
 
   render() {
-    // const abilities = this.state.attributes.abilities.map(ability => {
-    //   <li>{ability}</li>
-    // })
+    var abilities = []
+    var moves = []
+    var imgUrl = ''
+
+    if (this.state.attributes.id !== undefined) {
+      imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.state.attributes.id}.png`
+    }
+
+    if (this.state.attributes.abilities !== undefined) {
+      abilities = this.state.attributes.abilities.map((element, index) => 
+        <li key={index}>{element.ability.name}</li>
+      )
+    }
+
+    if (this.state.attributes.moves !== undefined) {
+      moves = this.state.attributes.moves.slice(0, 4).map((element, index) =>
+        <li key={index}>{element.move.name}</li>
+      )
+    }
+
+    
 
     return(
       <div className="card pokemon">
         <div className="card-image">
           <figure className="image is-128x128 is-centered">
             <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.state.attributes.id}.png`}
-              alt={this.state.attributes.name + " image"} />
+              src={imgUrl}
+              alt="pokemon" />
           </figure>
         </div>
         
@@ -37,12 +53,32 @@ class Pokemon extends Component {
           <div className="media-content">
             <p className="title is-4">{this.state.attributes.name}</p>
           </div>
-          <h4>Name: {this.state.attributes.name}</h4>
           
-          <label className="label">Abilities:</label>
-          <ol>
-            {/* {abilities} */}
-          </ol>
+          <table className="table is-narrow is-fullwidth">
+            <tbody>
+              <tr>
+                <td className="has-text-bold">Weight</td>
+                <td>{this.state.attributes.weight}</td>
+              </tr>
+
+              <tr>
+                <td className="has-text-bold">Height</td>
+                <td>{this.state.attributes.height}</td>
+              </tr>
+
+              <tr>
+                <td className="has-text-bold">Abilities</td>
+                <td>{ abilities }</td>
+              </tr>
+
+              <tr>
+                <td className="has-text-bold">Moves</td>
+                <td>{ moves }</td>
+              </tr>
+
+            </tbody>
+          </table>
+          
         </div>
             
         <footer className="card-footer">
